8d122bd90a7ed6e4172a64c32c09ff3c4026d77d
Bharadwaj <go2bharadwaj@gmail.com>
2020-10-08 22:25:36 -0700

    All files uploaded
    Uploading files again

---
+++ shop/Other files/RunShop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RunShop {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number of cheeses for shop:");
+		int max = input.nextInt();
+		ShopArr shop = new ShopArr(max);
+		shop.run();
+		System.out.println("Ran with Cheese Total: " + Cheese.numCheese);
+		input.close();
+	}
+
+}
+
